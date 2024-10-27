5761ab04dc55bc86aad434eb4d552f99e04f6bdb
Sabina <kasaevasab@mail.ru>
2024-10-27 19:06:46 +0300

    module 0 done

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -32,7 +32,103 @@ from typing import Callable, Iterable
 # $f(x) = |x - y| < 1e-2$
 
 
-# TODO: Implement for Task 0.1.
+def mul(x: float, y: float) -> float:
+    "$f(x, y) = x * y$"
+    return x * y
+
+
+def id(x: float) -> float:
+    "$f(x) = x$"
+    return x
+
+
+def add(x: float, y: float) -> float:
+    "$f(x, y) = x + y$"
+    return x + y
+
+
+def neg(x: float) -> float:
+    "$f(x) = -x$"
+    return -x
+
+
+def lt(x: float, y: float) -> float:
+    "$f(x) =$ 1.0 if x is less than y else 0.0"
+    return float(x < y)
+
+
+def eq(x: float, y: float) -> float:
+    "$f(x) =$ 1.0 if x is equal to y else 0.0"
+    return float(x == y)
+
+
+def max(x: float, y: float) -> float:
+    "$f(x) =$ x if x is greater than y else y"
+    return x if x > y else y
+
+
+def is_close(x: float, y: float) -> float:
+    "$f(x) = |x - y| < 1e-2$"
+    return float(abs(x - y) < 1e-2)
+
+
+def sigmoid(x: float) -> float:
+    r"""
+    $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$
+
+    (See https://en.wikipedia.org/wiki/Sigmoid_function )
+
+    Calculate as
+
+    $f(x) =  \frac{1.0}{(1.0 + e^{-x})}$ if x >=0 else $\frac{e^x}{(1.0 + e^{x})}$
+
+    for stability.
+    """
+    if x >= 0:
+        return 1.0 / (1.0 + math.exp(-x))
+    return math.exp(x) / (1.0 + math.exp(x))
+
+
+def relu(x: float) -> float:
+    """
+    $f(x) =$ x if x is greater than 0, else 0
+
+    (See https://en.wikipedia.org/wiki/Rectifier_(neural_networks) .)
+    """
+    return x * (x > 0)
+
+
+EPS = 1e-6
+
+
+def log(x: float) -> float:
+    "$f(x) = log(x)$"
+    return math.log(x + EPS)
+
+
+def exp(x: float) -> float:
+    "$f(x) = e^{x}$"
+    return math.exp(x)
+
+
+def log_back(x: float, d: float) -> float:
+    r"If $f = log$ as above, compute $d \times f'(x)$"
+    return d / (x + EPS)
+
+
+def inv(x: float) -> float:
+    "$f(x) = 1/x$"
+    return 1 / x
+
+
+def inv_back(x: float, d: float) -> float:
+    r"If $f(x) = 1/x$ compute $d \times f'(x)$"
+    return -d / (x ** 2 + EPS)
+
+
+def relu_back(x: float, d: float) -> float:
+    r"If $f = relu$ compute $d \times f'(x)$"
+    return d * (x > 0)
 
 
 # ## Task 0.3
@@ -51,4 +147,87 @@ from typing import Callable, Iterable
 # - prod: take the product of lists
 
 
-# TODO: Implement for Task 0.3.
+def map(fn: Callable[[float], float]) -> Callable[[Iterable[float]], Iterable[float]]:
+    """
+    Higher-order map.
+
+    See https://en.wikipedia.org/wiki/Map_(higher-order_function)
+
+    Args:
+        fn: Function from one value to one value.
+
+    Returns:
+         A function that takes a list, applies `fn` to each element, and returns a
+         new list
+    """
+    def map_fn(lst: Iterable[float]) -> Iterable[float]:
+        return [fn(el) for el in lst]
+    
+    return map_fn
+
+
+def negList(ls: Iterable[float]) -> Iterable[float]:
+    "Use `map` and `neg` to negate each element in `ls`"
+    return map(neg)(ls)
+
+
+def zipWith(
+    fn: Callable[[float, float], float]
+) -> Callable[[Iterable[float], Iterable[float]], Iterable[float]]:
+    """
+    Higher-order zipwith (or map2).
+
+    See https://en.wikipedia.org/wiki/Map_(higher-order_function)
+
+    Args:
+        fn: combine two values
+
+    Returns:
+         Function that takes two equally sized lists `ls1` and `ls2`, produce a new list by
+         applying fn(x, y) on each pair of elements.
+
+    """
+    def zipWith_fn(ls1: Iterable[float], ls2: Iterable[float]) -> Iterable[float]:
+        return [fn(el1, el2) for el1, el2 in zip(ls1, ls2)]
+
+    return zipWith_fn
+
+
+def addLists(ls1: Iterable[float], ls2: Iterable[float]) -> Iterable[float]:
+    "Add the elements of `ls1` and `ls2` using `zipWith` and `add`"
+    return zipWith(add)(ls1, ls2)
+
+
+def reduce(
+    fn: Callable[[float, float], float], start: float
+) -> Callable[[Iterable[float]], float]:
+    r"""
+    Higher-order reduce.
+
+    Args:
+        fn: combine two values
+        start: start value $x_0$
+
+    Returns:
+         Function that takes a list `ls` of elements
+         $x_1 \ldots x_n$ and computes the reduction :math:`fn(x_3, fn(x_2,
+         fn(x_1, x_0)))`
+    """
+    def reduce_fn(ls: Iterable[float]):
+        cur = start
+        for el in ls:
+            cur = fn(el, cur)
+        return cur
+
+    return reduce_fn
+
+
+
+def sum(ls: Iterable[float]) -> float:
+    "Sum up a list using `reduce` and `add`."
+    return reduce(add, 0)(ls)
+
+
+def prod(ls: Iterable[float]) -> float:
+    "Product of a list using `reduce` and `mul`."
+    return reduce(mul, 1.)(ls)
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -23,6 +23,8 @@ from minitorch.operators import (
     relu,
     relu_back,
     sigmoid,
+    exp,
+    is_close,
 )
 
 from .strategies import assert_close, small_floats
@@ -107,16 +109,21 @@ def test_sigmoid(a: float) -> None:
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert 0.0 <= sigmoid(a) <= 1.0
+    assert is_close(1 - sigmoid(a), sigmoid(-a))
+    assert sigmoid(0) == 0.5
+    assert sigmoid(a) < sigmoid(a + 0.1) + 1e-7
+
 
 
 @pytest.mark.task0_2
 @given(small_floats, small_floats, small_floats)
 def test_transitive(a: float, b: float, c: float) -> None:
     """Test the transitive property of less-than (a < b and b < c implies a < c)"""
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if lt(a, b) and lt(b, c):
+        assert lt(a, c ) == 1.0
+    else:
+        pass
 
 
 @pytest.mark.task0_2
@@ -124,8 +131,7 @@ def test_symmetric() -> None:
     """Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert mul(3.1, 2.56) == mul(2.56, 3.1)
 
 
 @pytest.mark.task0_2
@@ -133,15 +139,15 @@ def test_distribute() -> None:
     r"""Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    a, b, c = 3.1, 2.56, 1.4
+    assert mul(a, add(b, c)) == add(mul(a, b), mul(a, c))
 
 
 @pytest.mark.task0_2
 def test_other() -> None:
     """Write a test that ensures some other property holds for your functions."""
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    a, b = 3.1, 2.56
+    assert is_close(exp(add(a, b)), mul(exp(a), exp(b)))
 
 
 # ## Task 0.3  - Higher-order functions
@@ -168,8 +174,7 @@ def test_sum_distribute(ls1: List[float], ls2: List[float]) -> None:
     """Write a test that ensures that the sum of `ls1` plus the sum of `ls2`
     is the same as the sum of each element of `ls1` plus each element of `ls2`.
     """
-    # TODO: Implement for Task 0.3.
-    raise NotImplementedError("Need to implement for Task 0.3")
+    assert addLists(ls1, ls2) == [el1 + el2 for el1, el2 in zip(ls1, ls2)]
 
 
 @pytest.mark.task0_3
